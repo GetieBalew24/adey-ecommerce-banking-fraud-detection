4e01663f6f126972858ae8b7284afa760c1be486
GetieBalew24 <getiebalew20@gmail.com>
2025-02-06 23:52:15 +0300

    univarity analysis

--- scripts/visulaization.py
+++ scripts/visulaization.py
@@ -27,3 +27,19 @@ class DataVisualizer:
         """Visualizes the data using a pairplot to show relationships between features."""
         sns.pairplot(self.data)
         plt.show()
+    def plot_histogram(self, numerical_features):
+        """Plots histograms for the specified numerical features.
+
+        Args:
+            numerical_features (list): List of numerical feature names to plot.
+        """
+        plt.figure(figsize=(16, 5))
+        for i, feature in enumerate(numerical_features, 1):
+            plt.subplot(1, len(numerical_features), i)
+            sns.histplot(self.data[feature], bins=20, kde=True)
+            plt.title(f'Histogram for {feature}')
+            plt.xlabel(feature)
+            plt.ylabel('Frequency')
+        plt.tight_layout()
+        plt.show()
+        logg.info("Histograms plotted successfully!")
